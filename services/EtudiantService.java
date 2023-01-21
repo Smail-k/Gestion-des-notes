022b737dea140a65b4aad909b9497a6362c5cc3f
Ismail Kouidi <80042950+Smail-k@users.noreply.github.com>
2023-01-21 11:53:14 +0100

    Add files via upload

---
+++ services/EtudiantService.java
@@ -0,0 +1,28 @@
+package com.polytech.notes.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.polytech.notes.models.Etudiant;
+import com.polytech.notes.models.Note;
+import com.polytech.notes.models.Promotion;
+
+@Service
+public interface EtudiantService {
+
+	Etudiant saveEtudiant(Etudiant e);
+	Etudiant modifyEtudiant(Etudiant e);
+	void deleteEtudiantById(Etudiant e);
+	void deleteEtudiantByNumero(String numero);
+	Etudiant getEtudiantByNumero(String numero); 
+	Etudiant getNoteEtudiantByNumero(String numero,String annee);
+	Etudiant getNoteEtudiantByNom(String nom,String annee);
+	List<Note> getNoteSemestre(String nom,String prenom,String sem);
+	List<Note> getNoteAnnee(String nom,String prenom,String niveau);
+	
+	List<Etudiant> getEtudiants(Promotion p,Long annee);
+	List<String> getAnneeUniversitaires();
+	List<String> getPromotions();
+	
+}
